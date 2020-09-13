965952a5daa77c9fe1d1e91d6be9aa1caf0a16ac
malayarukilnihitha <71024838+malayarukilnihitha@users.noreply.github.com>
2020-09-13 19:26:30 +0530

    Create nihitha-app-ameaba.py

---
+++ nihitha-app-ameaba.py
@@ -0,0 +1,31 @@
+!pip install adafruit-io
+
+ x= "nihithamalayarukil"#ADAFRUIT_IO_USERNAME
+ y= "aio_hsRR46e4Mw8iG0CHiuUzYkJVkkWl"#ADAFRUIT_IO_KEY
+ 
+ from Adafruit_IO import Data
+ 
+aio = Client(x,y)
+feed=Feed(name='bot1')
+result=aio.create_feed(feed)
+result
+
+from Adafruit_IO import Data
+value=Data(value=0)
+value_send=aio.create_data('bot',value)
+!pip install python-telegram-bot
+from telegram.ext import Updater,CommandHandler
+import requests
+
+def get_url():
+    contents=requests.get('https://random.dog/woof.json').json()
+    url=contents['url']
+    return url
+
+def dog(bot,update):
+  url=get_url()
+  chat_id=update.message.chat_id
+  bot.send_photo(chat_id,photo=url)
+
+u=Updater('1235520242:AAF6YyIZNOfj3jU7dkBOaUFM-udZYg3oRB4')
+dp=u.dispatcher
